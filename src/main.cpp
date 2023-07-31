6b0f87eb03540c9e63d375e614dc82ebedd50159
YutoGCN <61838168+YutoGCN@users.noreply.github.com>
2023-07-31 17:12:53 +0900

    update spaces

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 #include "esp_log.h"
 
-static const char* TAG = "INIT";
+static const char *TAG = "INIT";
 
 #define SCK 27
 #define MISO 33
@@ -26,59 +26,59 @@ void setup()
   Serial.begin(115200);
 
   spibus.begin(VSPI, SCK, MISO, MOSI);
-  ESP_LOGV(TAG,"SPIBUS");
+  ESP_LOGV(TAG, "SPIBUS");
   pressureSensor.begin(&spibus, CS_PRESSURE, SPIFREQ);
-  ESP_LOGV(TAG,"pressure");
-  flash.begin(&spibus, CS_FLASH,SPIFREQ);
-  ESP_LOGV(TAG,"flash");
-  imu.begin(&spibus, CS_IMU,SPIFREQ);
-  ESP_LOGV(TAG,"IMU");
+  ESP_LOGV(TAG, "pressure");
+  flash.begin(&spibus, CS_FLASH, SPIFREQ);
+  ESP_LOGV(TAG, "flash");
+  imu.begin(&spibus, CS_IMU, SPIFREQ);
+  ESP_LOGV(TAG, "IMU");
 
-  ESP_LOGV(TAG,"whoAmI p:%d, IMU:%d, Flash:%d",pressureSensor.WhoAmI(),imu.WhoAmI(),128);
+  ESP_LOGV(TAG, "whoAmI p:%d, IMU:%d, Flash:%d", pressureSensor.WhoAmI(), imu.WhoAmI(), 128);
 
   uint8_t bf[256];
-  flash.read(0,bf);
+  flash.read(0, bf);
   for (int i = 0; i < 256; i++)
   {
-    Serial.printf("%x,",bf[i]);
+    Serial.printf("%x,", bf[i]);
   }
   Serial.println();
 
   for (int i = 0; i < 256; i++)
   {
-    bf[i] = 255-i;
+    bf[i] = 255 - i;
   }
 
-  flash.write(0,bf);
+  flash.write(0, bf);
 
   delay(1000);
 
-  flash.read(0,bf);
+  flash.read(0, bf);
   for (int i = 0; i < 256; i++)
   {
-    Serial.printf("%x,",bf[i]);
+    Serial.printf("%x,", bf[i]);
   }
   Serial.println();
 }
 
 void loop()
 {
-  uint8_t bf[3];
-  pressureSensor.Get(bf);
-  uint32_t marged = 0;
-  for (int i = 0; i < 3; i++)
-  {
-    marged += bf[i]<<(i*8);
-    printf("%d,\t",bf[i]);
-  }
-  printf("%d\r\n",marged);
+  // uint8_t bf[3];
+  // pressureSensor.Get(bf);
+  // uint32_t marged = 0;
+  // for (int i = 0; i < 3; i++)
+  // {
+  //   marged += bf[i]<<(i*8);
+  //   printf("%d,\t",bf[i]);
+  // }
+  // printf("%d\r\n",marged);
 
-  delay(1000);
+  // delay(1000);
 
-  if(Serial.available()){
-    uint8_t tmp = Serial.read();
-    if(tmp == 'r'){
-      ESP.restart();
-    }
-  }
+  // if(Serial.available()){
+  //   uint8_t tmp = Serial.read();
+  //   if(tmp == 'r'){
+  //     ESP.restart();
+  //   }
+  // }
 }
