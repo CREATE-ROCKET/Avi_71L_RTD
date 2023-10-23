44c5ae993209be779a14b244725ea3bf27e098a6
YutoGCN <61838168+YutoGCN@users.noreply.github.com>
2023-10-23 22:08:59 +0900

    add mode trasition

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "SPI_devices/ICM20602.h"
 #include "NEC920.hpp"
 #include "../RFparam/RTD_PARAM.h"
+#include "../../../Avi_71L_valveSystem/communication/gseCom.hpp"
 
 #include "esp_log.h"
 
@@ -38,9 +39,69 @@ namespace RTD_W_PINOUT
   constexpr uint8_t pin920Mode = 23;
 }
 
+namespace PWR_PINOUT
+{
+  HardwareSerial SER_PWR = Serial;
+  constexpr uint8_t SER_PWR_TX = 4;
+  constexpr uint8_t SER_PWR_RX = 5;
+}
+
+namespace VALVE_PINOUT
+{
+  HardwareSerial SER_VALVE = Serial2;
+  constexpr uint8_t SER_VALVE_TX = 16;
+  constexpr uint8_t SER_VALVE_RX = 17;
+}
+
+/**受信バッファ*/
+class rxBff
+{
+public:
+  uint8_t index = 0;
+  uint8_t data[128];
+};
+rxBff ValveRxBff;
+
+/** 受信用関数，パケット受信完了したらtrueを返す*/
+IRAM_ATTR bool
+recieve(HardwareSerial &SER, rxBff &rx)
+{
+  while (SER.available())
+  {
+    uint8_t tmp = SER.read();
+
+    if (rx.index == 0) /**ヘッダ受信*/
+    {
+      if (tmp == 0x43)
+      {
+        rx.data[rx.index++] = tmp;
+      }
+    }
+    else if ((rx.index == 1) || (rx.index == 2)) /**cmdidおよびlengthの受信*/
+    {
+      rx.data[rx.index++] = tmp;
+    }
+    else if (rx.index < (rx.data[2] - 1)) /**受信完了1個前までの処理*/
+    {
+      rx.data[rx.index++] = tmp;
+    }
+    else if (rx.index == rx.data[2] - 1) /**受信完了*/
+    {
+      rx.data[rx.index] = tmp;
+      rx.index = 0;
+      if (GseCom::checkPacket(rx.data) == 0)
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 void setup()
 {
   Serial.begin(115200);
+  VALVE_PINOUT::SER_VALVE.begin(115200, SERIAL_8N1, VALVE_PINOUT::SER_VALVE_RX, VALVE_PINOUT::SER_VALVE_TX);
 
   spibus.begin(VSPI, RTD_SPI_CONF::SCK, RTD_SPI_CONF::MISO, RTD_SPI_CONF::MOSI);
   ESP_LOGV(TAG, "SPIBUS");
@@ -62,152 +123,189 @@ void setup()
   ESP_LOGV(TAG, "NEC920 pin init");
 }
 
-bool booted = 0;
-uint8_t i = 0;
-uint8_t j = 0x71;
-
 void loop()
 {
-  if (i == 0)
+  // バルブ基板からの受信に対するコマンド処理系
+  if (recieve(VALVE_PINOUT::SER_VALVE, ValveRxBff))
   {
-    if (nec920.isBootFinished(400000) == 0)
-    {
-      ESP_LOGV(TAG, "booting...");
-    }
-    else if (booted == 0)
-    {
-      booted = 1;
-      i++;
-      ESP_LOGV(TAG, "boot finished");
-      ESP_LOGV(TAG, "NEC920 can send msg");
-      nec920.setRfConf(j, rtdRFparam::POWER, rtdRFparam::CHANNEL, rtdRFparam::RF_BAND, rtdRFparam::CS_MODE);
-      ESP_LOGV(TAG, "NEC920 set RF conf start");
-    }
-  }
+    uint8_t tmpCmdId = GseCom::getCmdId(ValveRxBff.data);
 
-  if (i == 1)
-  {
-    if (nec920.recieve())
+    if (tmpCmdId == 0x61)
     {
-      ESP_LOGV(TAG, "NEC920 recieve success");
-      if (nec920.isRecieveCmdResult())
+      uint8_t rxPayload[1];
+      uint8_t rxPayloadLength;
+      GseCom::getPayload(ValveRxBff.data, rxPayload, &rxPayloadLength);
+      uint8_t txPacket[5];
+      uint8_t txPayload = 0x00;
+      if (rxPayload[0] == 0x00)
       {
-        if (nec920.checkCmdResult(j++))
-        {
-          ESP_LOGV(TAG, "NEC920 set RF conf failed");
-          i++;
-          delay(1000);
-        }
-        else
-        {
-          ESP_LOGV(TAG, "NEC920 set RF conf success");
-          i++;
-          delay(1000);
-        }
+        // flight mode
+        txPayload = 0x40;
       }
-      nec920.dataUseEnd();
-    }
-    else
-    {
-
-      ESP_LOGV(TAG, "NEC920 recieve failed");
-      if (nec920.isModuleDeadByTimeout(1000000))
+      else if (rxPayload[0] == 0x01)
       {
-        ESP_LOGV(TAG, "NEC920 module dead");
-        i = 11;
+        // sleep mode
+        txPayload = 0x41;
+      }
+      else if (rxPayload[0] == 0x05)
+      {
+        // delete mode
+        txPayload = 0x45;
+      }
+      GseCom::makePacket(txPacket, 0x61, &txPayload, 1);
+      VALVE_PINOUT::SER_VALVE.write(txPacket, 5);
+
+      if (rxPayload[0] == 0x05)
+      {
+        // delete mode delete start
+        flash.erase();
+        txPayload = 0x46;
+        GseCom::makePacket(txPacket, 0x61, &txPayload, 1);
+        VALVE_PINOUT::SER_VALVE.write(txPacket, 5);
       }
     }
   }
 
-  if (i == 2)
-  {
-    if (nec920.canSendMsgCheck())
-    {
-      ESP_LOGV(TAG, "NEC920 can send msg");
-      uint8_t dstID[4] = {rtdRFparam::DST_1, rtdRFparam::DST_2, rtdRFparam::DST_3, rtdRFparam::DST_4};
-      uint8_t parameter[1] = {0x71};
-      nec920.sendTxCmd(0x13, j, dstID, parameter, 1);
-      i++;
-    }
-    else
-    {
-      ESP_LOGV(TAG, "NEC920 can't send msg");
-      if (nec920.isModuleDeadByTimeout(1000000))
-      {
-        ESP_LOGV(TAG, "NEC920 module dead");
-        i = 11;
-      }
-    }
-  }
+  // if (i == 0)
+  // {
+  //   if (nec920.isBootFinished(400000) == 0)
+  //   {
+  //     ESP_LOGV(TAG, "booting...");
+  //   }
+  //   else if (booted == 0)
+  //   {
+  //     booted = 1;
+  //     i++;
+  //     ESP_LOGV(TAG, "boot finished");
+  //     ESP_LOGV(TAG, "NEC920 can send msg");
+  //     nec920.setRfConf(j, rtdRFparam::POWER, rtdRFparam::CHANNEL, rtdRFparam::RF_BAND, rtdRFparam::CS_MODE);
+  //     ESP_LOGV(TAG, "NEC920 set RF conf start");
+  //   }
+  // }
 
-  if (i == 3)
-  {
-    if (nec920.recieve())
-    {
-      ESP_LOGV(TAG, "NEC920 recieve success");
+  // if (i == 1)
+  // {
+  //   if (nec920.recieve())
+  //   {
+  //     ESP_LOGV(TAG, "NEC920 recieve success");
+  //     if (nec920.isRecieveCmdResult())
+  //     {
+  //       if (nec920.checkCmdResult(j++))
+  //       {
+  //         ESP_LOGV(TAG, "NEC920 set RF conf failed");
+  //         i++;
+  //         delay(1000);
+  //       }
+  //       else
+  //       {
+  //         ESP_LOGV(TAG, "NEC920 set RF conf success");
+  //         i++;
+  //         delay(1000);
+  //       }
+  //     }
+  //     nec920.dataUseEnd();
+  //   }
+  //   else
+  //   {
 
-      if (nec920.isRecieveCmdData())
-      {
-        ESP_LOGV(TAG, "NEC920 recieve data");
-        uint8_t tmpArr[256];
-        uint8_t datalength = nec920.getRecieveData(tmpArr);
-        for (int i = 0; i < datalength; i++)
-        {
-          Serial.printf("%02X,", tmpArr[i]);
-        }
-        Serial.println();
-      }
+  //     ESP_LOGV(TAG, "NEC920 recieve failed");
+  //     if (nec920.isModuleDeadByTimeout(1000000))
+  //     {
+  //       ESP_LOGV(TAG, "NEC920 module dead");
+  //       i = 11;
+  //     }
+  //   }
+  // }
 
-      if (nec920.isRecieveCmdResult())
-      {
-        if (nec920.checkCmdResult(j++))
-        {
-          ESP_LOGV(TAG, "NEC920 send recieve failed");
-          i = 2;
-          delay(1000);
-        }
-        else
-        {
-          ESP_LOGV(TAG, "NEC920 send recieve success");
-          i = 2;
-          delay(1000);
-        }
-      }
+  // if (i == 2)
+  // {
+  //   if (nec920.canSendMsgCheck())
+  //   {
+  //     ESP_LOGV(TAG, "NEC920 can send msg");
+  //     uint8_t dstID[4] = {rtdRFparam::DST_1, rtdRFparam::DST_2, rtdRFparam::DST_3, rtdRFparam::DST_4};
+  //     uint8_t parameter[1] = {0x71};
+  //     nec920.sendTxCmd(0x13, j, dstID, parameter, 1);
+  //     i++;
+  //   }
+  //   else
+  //   {
+  //     ESP_LOGV(TAG, "NEC920 can't send msg");
+  //     if (nec920.isModuleDeadByTimeout(1000000))
+  //     {
+  //       ESP_LOGV(TAG, "NEC920 module dead");
+  //       i = 11;
+  //     }
+  //   }
+  // }
 
-      nec920.dataUseEnd();
-    }
-    else
-    {
-      ESP_LOGV(TAG, "NEC920 recieve failed");
-      if (nec920.isModuleDeadByTimeout(1000000))
-      {
-        ESP_LOGV(TAG, "NEC920 module dead");
-        i = 11;
-      }
-    }
-  }
+  // if (i == 3)
+  // {
+  //   if (nec920.recieve())
+  //   {
+  //     ESP_LOGV(TAG, "NEC920 recieve success");
 
-  if (i == 11)
-  {
-    ESP_LOGV(TAG, "reboot start");
-    nec920.startReboot();
-    i++;
-    booted = 0;
-  }
+  //     if (nec920.isRecieveCmdData())
+  //     {
+  //       ESP_LOGV(TAG, "NEC920 recieve data");
+  //       uint8_t tmpArr[256];
+  //       uint8_t datalength = nec920.getRecieveData(tmpArr);
+  //       for (int i = 0; i < datalength; i++)
+  //       {
+  //         Serial.printf("%02X,", tmpArr[i]);
+  //       }
+  //       Serial.println();
+  //     }
 
-  if (i == 12)
-  {
+  //     if (nec920.isRecieveCmdResult())
+  //     {
+  //       if (nec920.checkCmdResult(j++))
+  //       {
+  //         ESP_LOGV(TAG, "NEC920 send recieve failed");
+  //         i = 2;
+  //         delay(1000);
+  //       }
+  //       else
+  //       {
+  //         ESP_LOGV(TAG, "NEC920 send recieve success");
+  //         i = 2;
+  //         delay(1000);
+  //       }
+  //     }
 
-    if (nec920.doReboot(100000))
-    {
-      ESP_LOGV(TAG, "rebooting...");
-    }
-    else
-    {
-      ESP_LOGV(TAG, "reboot finished");
-      i = 0;
-    }
-  }
+  //     nec920.dataUseEnd();
+  //   }
+  //   else
+  //   {
+  //     ESP_LOGV(TAG, "NEC920 recieve failed");
+  //     if (nec920.isModuleDeadByTimeout(1000000))
+  //     {
+  //       ESP_LOGV(TAG, "NEC920 module dead");
+  //       i = 11;
+  //     }
+  //   }
+  // }
 
-  delay(10);
+  // if (i == 11)
+  // {
+  //   ESP_LOGV(TAG, "reboot start");
+  //   nec920.startReboot();
+  //   i++;
+  //   booted = 0;
+  // }
+
+  // if (i == 12)
+  // {
+
+  //   if (nec920.doReboot(100000))
+  //   {
+  //     ESP_LOGV(TAG, "rebooting...");
+  //   }
+  //   else
+  //   {
+  //     ESP_LOGV(TAG, "reboot finished");
+  //     i = 0;
+  //   }
+  // }
+
+  // delay(10);
 }
